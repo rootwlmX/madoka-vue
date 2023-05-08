69322e105d9c8692a38a9d2c03e3cbe6b962441d
Arcueid <1170895958@qq.com>
2023-05-08 14:57:55 +0800

    feat: github oauth

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,6 +1,6 @@
 const GITHUB_OAUTH = {
-  url: 'https://github.com/login/oauth/authorize?client_id=7dd33c1a56813db7f797&state=blog',
-  clientid: '7dd33c1a56813db7f797'
+  url: 'https://github.com/login/oauth/authorize?client_id=d36422ab56e8ee83ebd0&state=blog',
+  clientid: 'd36422ab56e8ee83ebd0'
 }
 const WEIBO_OAUTH = {
   url: 'https://api.weibo.com/oauth2/authorize?client_id=1075586328&redirect_uri=http://aimeeserver.mangoya.cn:8899/v1/user/login&response_type=code&state=weibo_blog',
